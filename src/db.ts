b3bd614a30bc24c58b8c191e2772da0c777f951e
Bianca91 <biancasteyn91@gmail.com>
2018-04-11 15:11:27 +0200

    Fix conflicts

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,19 @@
-import { createConnection } from "typeorm";
-import { DefaultNamingStrategy } from "typeorm/naming-strategy/DefaultNamingStrategy";
-import { NamingStrategyInterface } from "typeorm/naming-strategy/NamingStrategyInterface";
-import { snakeCase } from "typeorm/util/StringUtils";
-import User from "./entities/user";
+import { createConnection } from 'typeorm'
+import { DefaultNamingStrategy } from 'typeorm/naming-strategy/DefaultNamingStrategy'
+import { NamingStrategyInterface } from 'typeorm/naming-strategy/NamingStrategyInterface'
+import { snakeCase } from 'typeorm/util/StringUtils'
+import {Delivery} from './entities/delivery'
+import {Order} from './entities/order'
 
-class CustomNamingStrategy extends DefaultNamingStrategy
-  implements NamingStrategyInterface {
+
+class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
+
   tableName(targetName: string, userSpecifiedName: string): string {
-    return userSpecifiedName ? userSpecifiedName : snakeCase(targetName) + "s";
+    return userSpecifiedName ? userSpecifiedName : snakeCase(targetName) + 's';
   }
 
-  columnName(
-    propertyName: string,
-    customName: string,
-    embeddedPrefixes: string[]
-  ): string {
-    return snakeCase(
-      embeddedPrefixes.concat(customName ? customName : propertyName).join("_")
-    );
+  columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
+    return snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
   }
 
   columnNameCustomized(customName: string): string {
@@ -32,11 +28,13 @@ class CustomNamingStrategy extends DefaultNamingStrategy
 export default () =>
   createConnection({
     type: "postgres",
-    url:
-      process.env.DATABASE_URL ||
-      "postgres://postgres:secret@localhost:5432/postgres",
-    entities: [User],
+    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
+    entities: [
+      Order,
+      Delivery
+    ],
     synchronize: true,
     logging: true,
     namingStrategy: new CustomNamingStrategy()
-  }).then(_ => console.log("Connected to Postgres with TypeORM"));
+  })
+    .then(_ => console.log('Connected to Postgres with TypeORM'))
